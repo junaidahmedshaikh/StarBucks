4f4b99c6e0031b6ba8c704dd70154290e192d9f5
Junaid Ahmed Shaikh <junaiddeveloperofficial@gmail.com>
2022-11-26 12:01:00 -1200

    Website

---
+++ findastore/map.js
@@ -0,0 +1,21 @@
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: -25.344, lng: 131.031 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+  }
+  
+  window.initMap = initMap;
+
+  var map_parameters = { center: {lat: 47.490, lng: -117.585}, zoom: 8 };
+                var map = new google.maps.Map(document.getElementById('map'), map_parameters);
+  

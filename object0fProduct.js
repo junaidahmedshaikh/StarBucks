ba2ea142c46c0cb6f515bedf027345bd9ff9b62a
Junaid Ahmed Shaikh <junaiddeveloperofficial@gmail.com>
2022-11-29 12:29:32 -1200

    version 1.2
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # On branch main
    # Your branch is behind 'origin/main' by 1 commit, and can be fast-forwarded.
    #   (use "git pull" to update your local branch)
    #
    # Changes to be committed:
    #	new file:   StarBucks
    #	new file:   Starbucks-Coffee-at-Home-New-Packaging_Knockout-Image.jpg
    #	new file:   contact/contact.html
    #	modified:   index.html
    #	modified:   main.css
    #	new file:   object0fProduct.js
    #	new file:   responsiveCSS.css
    #	new file:   whatsnew/whatsnew.html
    #

---
+++ object0fProduct.js
@@ -0,0 +1,175 @@
+
+let coffeeAndtea = [
+    {
+       id: "CC",
+        image: "",
+        name: "Cold Coffee",
+        price: "180rs",
+        kcal: "190",
+        detail: "lorem onmev onm",
+    },
+    {
+    
+        id: "C1",
+        image: "",
+        name: "Coffee",
+        price: "100rs",
+        kcal: "180",
+        detail: "lorem onmev onm",
+    },
+    {
+    
+        id: "HT",
+        image: "https://res.cloudinary.com/difvkvxuy/image/upload/v1669394361/Starbucks/product/1/IMG_4981-scaled_zztmna.jpg",
+        name: "Hot Tea",
+        price: "110rs",
+        kcal: "660",
+        detail: "lorem onmev onm",
+    },
+    {
+    
+        id: "GT",
+        image: "",
+        name: "Green Tea",
+        price: "120rs",
+        kcal: "60",
+        detail: "lorem onmev onm",
+    },
+    {
+    
+        id: "IT",
+        image: "https://res.cloudinary.com/difvkvxuy/image/upload/v1669394360/Starbucks/product/1/IMG_4980-scaled_f7fmq5.jpg",
+        name: "Ice Tea",
+        price: "120rs",
+        kcal: "90",
+        detail: "lorem onmev onm",
+    },
+    {
+    
+        id: "MT",
+        image: "https://res.cloudinary.com/difvkvxuy/image/upload/v1669394343/Starbucks/product/1/IMG_4987-scaled_i395ig.jpg",
+        name: "Masala Tea",
+        price: "120rs",
+        kcal: "90",
+        detail: "lorem onmev onm",
+    },
+]
+
+// Drink Section
+let drinks = [
+    {
+
+        id: "CIC",
+        image: "https://res.cloudinary.com/difvkvxuy/image/upload/v1669394002/Starbucks/product/Starbucks-Cup-PNG-Image_igs178.png",
+        name: "Coco Ice Cream",
+        price: "320rs",
+        kcal: "1000",
+        detail: "lorem onmev onm",
+    },
+    {
+    
+        id: "CC2",
+        image: "https://res.cloudinary.com/difvkvxuy/image/upload/v1669394007/Starbucks/product/Starbucks-Cup-PNG-Free-Download_aqb8pv.png",
+        name: "Coca Cola",
+        price: "130rs",
+        kcal: "990",
+        detail: "lorem onmev onm",
+    },
+    {
+    
+        id: "HC",
+        image: "https://res.cloudinary.com/difvkvxuy/image/upload/v1669393986/Starbucks/product/Starbucks-Cup-PNG-Picture_z6p81c.png",
+        name: "Hot Chocalate",
+        price: "220rs",
+        kcal: "920",
+        detail: "lorem onmev onm",
+    },
+]
+// Cake Section 
+let cake = [
+    {
+    
+        id: "LC",
+        image: "https://res.cloudinary.com/difvkvxuy/image/upload/v1669394373/Starbucks/product/1/64_mejxwz.jpg",
+        name: "Lemon Cake",
+        price: "2000rs",
+        kcal: "1190",
+        detail: "lorem onmev onm",
+    },
+    {
+    
+        id: "DC",
+        image: "https://res.cloudinary.com/difvkvxuy/image/upload/v1669394312/Starbucks/product/1/IMG_3154-600x800_jcmiat.jpg",
+        name: "Dry Cake",
+        price: "4000rs",
+        kcal: "2420",
+        detail: "lorem onmev onm",
+    },
+    {
+    
+        id: "CC",
+        image: "https://res.cloudinary.com/difvkvxuy/image/upload/v1669394369/Starbucks/product/1/85CE350C-B05A-4C92-8F43-8B5DE9A57E4E-600x800_noh2db.jpg",
+        name: "Cream Cake",
+        price: "1880rs",
+        kcal: "5890",
+        detail: "lorem onmev onm",
+    },
+]
+
+// Other Products
+let other = [
+    {
+    
+        id: "ED",
+        image: "",
+        name: "Starbucks Epresso Dark",
+        price: "290rs",
+        kcal: "none",
+        detail: "lorem onmev onm",
+    }, 
+    {
+    
+        id: "EDR",
+        image: "",
+        name: "Starbucks Epresso Dark Roast",
+        price: "490rs",
+        kcal: "none",
+        detail: "lorem onmev onm",
+    }, 
+    {
+    
+        id: "EB",
+        image: "",
+        name: "Starbucks Epresso Blonde",
+        price: "350rs",
+        kcal: "none",
+        detail: "lorem onmev onm",
+    },    
+    {
+    
+        id: "SC",
+        image: "",
+        name: "Starbucks Cup",
+        price: "50rs",
+        kcal: "none",
+        detail: "lorem onmev onm",
+    }, 
+    {
+    
+        id: "CP",
+        image: "",
+        name: "Coffee Powder",
+        price: "1000rs",
+        kcal: "none",
+        detail: "lorem onmev onm",
+    }, 
+    {
+    
+        id: "CP2",
+        image: "",
+        name: "Coffee Powder 2",
+        price: "290rs",
+        kcal: "none",
+        detail: "lorem onmev onm",
+    }, 
+]
